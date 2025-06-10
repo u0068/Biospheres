da9c6448dd371005d2e5a8e3942e8cf9d712ba84
u0068 <g3nio15@gmail.com>
2025-06-10 13:39:26 +0100

    Revert "Merging and consistency"
    This reverts commit f92a688968dee077106cc2b80d9c3109dace28eb.

--- timer.cpp
+++
@@ -1,4 +0,0 @@
-#include "timer.h"
-
-// Everything in timer.h is trivial, so we don't need to implement anything here for now.
-// If you do need to change anything in timer.h, move it to here and implement it.
