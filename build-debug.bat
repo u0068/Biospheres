3f7188899500303221f791e3eb40fac022ff7093
Quadraxis77 <Quadraxis77@gmail.com>
2025-06-16 07:39:55 -0700

    Update for VSC compatibility

--- build-debug.bat
+++ build-debug.bat
@@ -1,7 +1,7 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 echo Building Biospheres 2 - Debug Configuration...
-msbuild CellSim.vcxproj /p:Configuration=Debug /p:Platform=x64 /p:SolutionDir="%~dp0" /verbosity:minimal
+msbuild CellSim.vcxproj /p:Configuration=Debug /p:Platform=x64 /p:SolutionDir="%~dp0\" /verbosity:minimal
 if %ERRORLEVEL% EQU 0 (
     echo Build succeeded!
 ) else (
